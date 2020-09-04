f3a134115ddbfa22a98765daa18f1adab035a763
Biniti <70482627+Biniti@users.noreply.github.com>
2020-09-04 14:20:22 -0300

    Update Simulador de dado.py

--- Simulador de dado.py
+++ Simulador de dado.py
@@ -2,8 +2,8 @@ dado= input("Deseja jogar o dado ?" "\n")
 dado.upper()
 while dado != "não":
     if dado == "sim":
-        from random import randint//Essa parte serve para receber números aleatórios
-        print("O valor do dado é:",randint(1,6))//''randit(1,6)'' estou definindo valores aleatórios entre 1 a 6
+        from random import randint #Essa parte serve para receber números aleatórios
+        print("O valor do dado é:",randint(1,6)) #''randit(1,6)'' estou definindo valores aleatórios entre 1 a 6
         dado = input("Deseja jogar o dado ?" "\n")
         dado.upper()
     else:
